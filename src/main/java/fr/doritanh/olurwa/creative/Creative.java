ecbe51ef3d72f1ecc4660d6683babaec5314c030
Doritanh <charrier.antho99@gmail.com>
2021-03-02 21:11:09 +0100

    Remove player join and player quit

--- src/main/java/fr/doritanh/olurwa/creative/Creative.java
+++ src/main/java/fr/doritanh/olurwa/creative/Creative.java
@@ -2,6 +2,25 @@ package fr.doritanh.olurwa.creative;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import fr.doritanh.olurwa.creative.listeners.PlayerListener;
+
 public class Creative extends JavaPlugin {
 
+	private static Creative instance;
+	
+	public Creative() {
+		instance = this;
+	}
+	
+	@Override
+	public void onEnable() {
+		this.getServer().getPluginManager().registerEvents(new PlayerListener(), this);
+	}
+	
+	@Override
+	public void onDisable() {}
+	
+	public static Creative get() {
+		return Creative.instance;
+	}
 }
---
+++ src/main/java/fr/doritanh/olurwa/creative/listeners/PlayerListener.java
@@ -0,0 +1,22 @@
+package fr.doritanh.olurwa.creative.listeners;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+public class PlayerListener implements Listener {
+
+	@SuppressWarnings("deprecation")
+	@EventHandler
+	public void onPlayerJoin(PlayerJoinEvent e) {
+		e.setJoinMessage(null);
+	}
+	
+	@SuppressWarnings("deprecation")
+	@EventHandler
+	public void onPlayerQuit(PlayerQuitEvent e) {
+		e.setQuitMessage(null);
+	}
+	
+}

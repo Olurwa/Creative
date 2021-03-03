1de9fc88318f18d7b464c57a95678cea6e456b33
Doritanh <charrier.antho99@gmail.com>
2021-03-03 12:53:55 +0100

    Add tablist to creative

--- src/main/java/fr/doritanh/olurwa/creative/Creative.java
+++ src/main/java/fr/doritanh/olurwa/creative/Creative.java
@@ -1,26 +1,57 @@
 package fr.doritanh.olurwa.creative;
 
+import org.bukkit.World;
+import org.bukkit.World.Environment;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import fr.doritanh.olurwa.creative.listeners.BungeeMessageListener;
+import fr.doritanh.olurwa.creative.listeners.CoreMessageListener;
 import fr.doritanh.olurwa.creative.listeners.PlayerListener;
+import fr.doritanh.olurwa.creative.tablist.TabList;
 
 public class Creative extends JavaPlugin {
 
 	private static Creative instance;
-	
+
+	private World world;
+	private TabList tablist;
+
 	public Creative() {
 		instance = this;
 	}
-	
+
 	@Override
 	public void onEnable() {
 		this.getServer().getPluginManager().registerEvents(new PlayerListener(), this);
+
+		// Register channels
+		this.getServer().getMessenger().registerOutgoingPluginChannel(this, "BungeeCord");
+		this.getServer().getMessenger().registerIncomingPluginChannel(this, "BungeeCord", new BungeeMessageListener());
+		this.getServer().getMessenger().registerOutgoingPluginChannel(this, "olurwa:core");
+		this.getServer().getMessenger().registerIncomingPluginChannel(this, "olurwa:core", new CoreMessageListener());
+
+		for (World w : this.getServer().getWorlds()) {
+			if (w.getEnvironment() == Environment.NORMAL) {
+				this.world = w;
+			}
+		}
+
+		this.tablist = new TabList();
 	}
-	
+
 	@Override
-	public void onDisable() {}
-	
+	public void onDisable() {
+	}
+
 	public static Creative get() {
 		return Creative.instance;
+	}
+
+	public World getWorld() {
+		return this.world;
+	}
+
+	public TabList getTabList() {
+		return this.tablist;
 	}
 }
